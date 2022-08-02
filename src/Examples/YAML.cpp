6ef9940a1c7296fba9051fb26fe2cb44d69d1845
Mag1str0 <rysin_d@mail.ru>
2022-08-03 01:11:43 +0300

    Added precompiled yaml-cpp lib.

--- src/Examples/YAML.cpp
+++ src/Examples/YAML.cpp
@@ -3,7 +3,6 @@
 int main()
 {
     std::ofstream stream("test.yml");
-    YAML::Node primes;
-    stream << primes;
-    //  = YAML::Load("[2, 3, 5, 7, 11]");
+    YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
+    stream << primes; 
 }
