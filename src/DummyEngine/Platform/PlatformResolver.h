0ffd0f685381cee6e9887c316fa9bf859ff5d395
Mag1str0 <rysin_d@mail.ru>
2023-01-11 12:39:07 +0300

    Initital.

---
+++ src/DummyEngine/Platform/PlatformResolver.h
@@ -0,0 +1,14 @@
+
+#ifdef _WIN32
+	#ifdef _WIN64
+		#define DE_PLATFORM_WINDOWS
+	#else
+		#error "x86 not supported."
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#error "Apple not supported."
+#elif defined(__linux__)
+	#define DE_PLATFORM_LINUX
+#else
+	#error "Unknown platform."
+#endif 
