8d8a8332fec5c77b1e1fd15742eaa74e058ac2e5
baalaandaa <baalaandaa@gmail.com>
2023-05-15 01:35:27 +0300

    Undo commit build-release

--- cmake-build-release/src/DummyEngine/Libs/ASSIMP/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/balanda/DummyEngine/src/DummyEngine/Libs/ASSIMP/contrib/zlib
-# Build directory: C:/Users/balanda/DummyEngine/cmake-build-release/src/DummyEngine/Libs/ASSIMP/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
