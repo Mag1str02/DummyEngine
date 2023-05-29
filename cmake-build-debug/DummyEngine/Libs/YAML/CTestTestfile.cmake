fa9711abe9352145cc6c59b93aa464ed781516c2
SteveBalayanAKAMedian <steve.balayan@gmail.com>
2023-05-29 23:03:24 +0300

    audio engine baseline

--- cmake-build-debug/DummyEngine/Libs/YAML/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Kursach/DummyEngine/DummyEngine/Libs/YAML
-# Build directory: D:/Kursach/DummyEngine/cmake-build-debug/DummyEngine/Libs/YAML
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
--- cmake-build-release/DummyEngine/Libs/YAML/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Kursach/DummyEngine/DummyEngine/Libs/YAML/util
-# Build directory: D:/Kursach/DummyEngine/cmake-build-release/DummyEngine/Libs/YAML/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
