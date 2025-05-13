312b61afb814e091335c7c80dbd7ca1fee5fe601
Mag1str02 <rysin_d@mail.ru>
2025-05-13 22:10:54 +0300

    More concurrency

--- DummyEngine/Core/Application/Concurrency.cpp
+++ DummyEngine/Core/Application/Concurrency.cpp
@@ -5,16 +5,22 @@ namespace DummyEngine {
     SINGLETON_BASE(Concurrency);
     S_INITIALIZE() {
         engine_main_thread_pool_.Start();
+        back_ground_thread_pool_.Start();
         return Unit();
     }
     S_TERMINATE() {
+        back_ground_thread_pool_.Stop();
         engine_main_thread_pool_.Stop();
         return Unit();
     }
 
-    S_METHOD_IMPL(ThreadPool&, GetEngineMainScheduler, (), ()) {
+    S_METHOD_IMPL(IScheduler&, GetEngineMainScheduler, (), ()) {
         return engine_main_thread_pool_;
     }
+    S_METHOD_IMPL(IScheduler&, GetEngineBackgroundScheduler, (), ()) {
+        return back_ground_thread_pool_;
+    }
+
     S_METHOD_IMPL(ExternalThreadPool&, GetMainThreadScheduler, (), ()) {
         return main_thread_scheduler_;
     }
--- DummyEngine/Core/Application/Concurrency.h
+++ DummyEngine/Core/Application/Concurrency.h
@@ -8,11 +8,13 @@ namespace DummyEngine {
     class Concurrency : public Singleton<Concurrency> {
         SINGLETON(Concurrency)
     public:
-        S_METHOD_DEF(ThreadPool&, GetEngineMainScheduler, ());
+        S_METHOD_DEF(IScheduler&, GetEngineMainScheduler, ());
+        S_METHOD_DEF(IScheduler&, GetEngineBackgroundScheduler, ());
         S_METHOD_DEF(ExternalThreadPool&, GetMainThreadScheduler, ());
 
     private:
         ExternalThreadPool main_thread_scheduler_;
-        ThreadPool         engine_main_thread_pool_ = ThreadPool(1);
+        ThreadPool         engine_main_thread_pool_  = ThreadPool(1);
+        ThreadPool         back_ground_thread_pool_ = ThreadPool(std::thread::hardware_concurrency());
     };
 }  // namespace DummyEngine
